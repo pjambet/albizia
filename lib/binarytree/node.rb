5d4473ec1a56813a92ce2b1672dba20edad1910e
pjambet <pierre.jambet@gmail.com>
2012-09-27 01:30:28 +0200

    remove white spaces

--- lib/binarytree/node.rb
+++ lib/binarytree/node.rb
@@ -1,35 +1,35 @@
 module Binarytree
   class Node
-    
+
     attr_accessor :left_child, :right_child, :value, :parent
-    
+
     def initialize(v=nil)
       @value = v
       @parent = nil
     end
-    
+
     def empty?
       leaf? && value == nil
     end
-    
+
     # A leaf node has no children
     def leaf?
       @left_child == nil && @right_child == nil
     end
-    
+
     # A root has no parent
     def root?
       @parent == nil
     end
-    
+
     #The depth of a node n is the length of the path from the root to the node.
     # The set of all nodes at a given depth is sometimes called a level of the
     # tree. The root node is at depth zero (here 1).
-    def depth      
-      
+    def depth
+
     end
-    
-    # The height of a tree is the length of the path from the root to the 
+
+    # The height of a tree is the length of the path from the root to the
     # deepest node in the tree. A (rooted) tree with only one node (the root)
     # has a height of zero (here it's 1).
     def height
@@ -38,11 +38,11 @@ module Binarytree
       else
         init_depth = 1
       end
-      
+
       if empty?
         -1
       elsif root? && leaf?
-        0      
+        0
       elsif leaf?
         1
       else
@@ -57,17 +57,17 @@ module Binarytree
         end
       end
     end
-    
+
     # Siblings are nodes that share the same parent node.
     def sibling
-      
+
     end
-    
+
     # The size of a node is the number of descendants it has including itself.
     def size
-      
+
     end
-    
+
     def max
       if @right_child == nil
         if empty?
@@ -79,7 +79,7 @@ module Binarytree
         @right_child.max
       end
     end
-    
+
     def min
       if @left_child == nil
         if empty?
@@ -91,7 +91,7 @@ module Binarytree
         @left_child.min
       end
     end
-    
+
     def add(v)
       if empty?
         @value = v
@@ -111,11 +111,11 @@ module Binarytree
         end
       end
     end
-    
+
     # Alias of #add
     def <<(v)
       add(v)
     end
-    
+
   end #class
 end #module
--- spec/binarytree/rb_node_spec.rb
+++ spec/binarytree/rb_node_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 module Redblack
   describe RBNode do
-    
+
   end
 end
