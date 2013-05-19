46c193e32ef82fd1fc3b750cbe9f4b9d77329062
pjambet <pierre.jambet@gmail.com>
2013-05-19 12:15:23 -0400

    Add basic RedBlack implementation

--- lib/albizia/rb_node.rb
+++ lib/albizia/rb_node.rb
@@ -1,5 +1,40 @@
 module Albizia
-  class RBNode
+  class RBNode < Node
+    COLORS = %i(red black)
+    attr_accessor :color
+
+    def initialize(value)
+      super(value)
+      @color = :black
+    end
+
+    def black?
+      color == :black
+    end
+
+    def red?
+      color == :red
+    end
+
+    def valid?
+      traverse do |node|
+        return false unless node.red? || node.black?
+        return false if node.leaf? && !node.black?
+        return false if node.red? and node.right_child.red? and
+          node.left_child.red?
+        # Every simple path from a node to a descendant leaf contains the same
+        # number of black nodes.
+      end
+      true
+    end
+
+    class Sentinel < RBNode
+      attr_reader :value
+
+      def initialize
+        @value = nil
+      end
+    end
 
   end
 end
--- spec/albizia/rb_node_spec.rb
+++ spec/albizia/rb_node_spec.rb
@@ -2,6 +2,10 @@ require "spec_helper"
 
 module Albizia
   describe RBNode do
+    subject(:node) { RBNode.new 10 }
 
+    it { should be_black }
+    it { should be_leaf }
+    it { should be_valid }
   end
 end
